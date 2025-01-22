8a55cb48d2e891d735597c76c14cbb260380f98b
Analyser <analyser@gmail.com>
2025-01-22 11:23:26 +0800

    Replace Ubuntu code in dir name with 'ubuntu'
    and:
    - update inner Ubuntu image code from 'jammy' to 'noble'.

--- 9/9-jdk17/9-jdk17-temurin-ubuntu/build-sample.sh
+++ 9/9-jdk17/9-jdk17-temurin-ubuntu/build-sample.sh
@@ -8,4 +8,4 @@ docker buildx build \
 	--build-arg HTTP_PROXY=http://192.168.199.21:1087 \
 	--build-arg HTTPS_PROXY=http://192.168.199.21:1087 \
 	--build-arg NO_PROXY=192.168.*,10.*,172.*,mirrors.tuna.tsinghua.edu.cn,mirrors.aliyun.com,*.aliyun.com,*.aliyuncs.com,*.dianplus.cn,*.dianjia.io,*.taobao.com \
-	-t registry.cn-hangzhou.aliyuncs.com/dianplus/tomcat:9-jdk17-temurin-jammy ./
+	-t registry.cn-hangzhou.aliyuncs.com/dianplus/tomcat:9-jdk17-temurin-noble ./
